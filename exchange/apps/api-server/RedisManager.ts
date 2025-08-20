eabde0cc59e7623111532e9b9789df8ddcbb48c0
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-08-20 18:58:25 +0530

    updated the main

--- exchange/apps/api-server/RedisManager.ts
+++ exchange/apps/api-server/RedisManager.ts
@@ -3,16 +3,35 @@ import type { RedisClientType } from "redis";
 
 class RedisClient{
     private publisher: RedisClientType;
-    private subsciber: RedisClientType;
+    private subscriber: RedisClientType;
+    private static instance: RedisClient
     constructor(){
         this.publisher = createClient();
-        this.subsciber = createClient();
+        this.subscriber = createClient();
         this.connect()
     }
 
     private async connect(){
         await this.publisher.connect();
-        await this.subsciber.connect()
+        await this.subscriber.connect()
+    }
+
+    public static getInstance(){
+        if(!this.instance){
+            this.instance = new RedisClient;
+        }
+        return this.instance
+    }
+
+    public publishSubscribe(message: string){
+        const id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        return new Promise<string>((resolve)=>{
+            this.subscriber.subscribe(id, (message)=>{
+                this.subscriber.unsubscribe(id)
+                resolve(JSON.parse(message))
+            })
+            this.publisher.lPush("ToEngine", JSON.stringify({id, message}))
+        })
     }
 
 }
