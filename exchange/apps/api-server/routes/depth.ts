0f5d13306170e5cc4debcf58cf011ba87235b8a6
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-08-21 23:50:03 +0530

    updated depth

--- exchange/apps/api-server/routes/depth.ts
+++ exchange/apps/api-server/routes/depth.ts
@@ -1,7 +1,19 @@
+import {RedisClient} from '../RedisManager'
 import express from 'express'
+import type { MessageFromEngine } from 'types';
 export const depthRouter = express.Router()
 
-depthRouter.get('/depth', (req, res)=>{
-    const {symbol} = req.query
-
+depthRouter.get('/depth', async (req, res)=>{
+    try{
+        const {symbol} = req.query;
+        const response: MessageFromEngine = await RedisClient.getInstance().publishSubscribe({
+            type: 'GET_DEPTH',
+            data:{
+                market: symbol as string
+            }
+        })
+        res.json(response)
+    }catch(error){
+        res.json(error)
+    }
 })
