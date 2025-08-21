d44d0c55f37a8bf9e53157bfffcb5b3108883965
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-08-21 15:19:09 +0530

    updated the order router

--- exchange/apps/api-server/routes/order.ts
+++ exchange/apps/api-server/routes/order.ts
@@ -2,9 +2,26 @@ import express from 'express'
 export const orderRouter = express.Router()
 import { middleware } from '../middleware'
 import {RedisClient} from '../RedisManager'
-import type {MessageToEngine, Order} from 'types'
+import type {MessageFromEngine} from 'types'
 
 orderRouter.post('/order', middleware, async (req, res)=>{
-    const message: MessageToEngine = req.body.message
-    RedisClient.getInstance().publishSubscribe(message)
+    const { market, price, quantity, side, userId } = req.body;
+    const response: MessageFromEngine = await RedisClient.getInstance().publishSubscribe({
+        type: 'CREATE_ORDER',
+        data: {
+            market,
+            price,
+            quantity,
+            side,
+            userId
+        }
+    });
+    res.json(response.data);
+})
+
+orderRouter.get('/order', middleware, async (req, res)=>{
+    const market = req.query.market
+    const userId = req.userId
+
+    
 })
